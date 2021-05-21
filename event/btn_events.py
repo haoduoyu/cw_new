608930d2bd2ca74596331088e09ed880143717c1
haoduoyu <493123129@qq.com>
2021-05-21 10:59:43 +0800

    Add files via upload

---
+++ event/btn_events.py
@@ -0,0 +1,57 @@
+class BtnEvents:
+
+    @staticmethod
+    def open_camera_btn_event():
+        """
+        响应开启摄像头事件
+        :return:
+        """
+        print('开启摄像头')
+
+    @staticmethod
+    def pet_kind_btn_event(select_value):
+        """
+        响应宠物类型事件
+        :return:
+        """
+        print('响应宠物类型', select_value)
+
+    @staticmethod
+    def select_contract_btn_event():
+        """
+        响应选择联系人事件
+        :return:
+        """
+        print('选择联系人')
+
+    @staticmethod
+    def open_monitor_btn_event():
+        """
+        响应开启实时监控事件
+        :return:
+        """
+        print('开启实时监控')
+
+    @staticmethod
+    def analyse_pic_btn_event():
+        """
+        响应分析图片事件
+        :return:
+        """
+        print('分析图片')
+
+    @staticmethod
+    def test_info_btn_event():
+        """
+        响应测试信息事件
+        :return:
+        """
+        print('测试信息')
+
+    @staticmethod
+    def personal_info_btn_event():
+        """
+        响应个人信息事件
+        :return:
+        """
+        print('个人事件')
---
+++ main.py
@@ -0,0 +1,16 @@
+
+import sys
+from view import main_view
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+
+    ui = main_view.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    MainWindow.show()
+    sys.exit(app.exec_())
+
